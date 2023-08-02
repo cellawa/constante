b198cbe8fd7f5beebdc87ad0325a0d5c78fc30df
wallace <w.riosmagalhaes@gmail.com>
2023-08-02 14:59:26 -0300

    first commit

---
+++ exemplo02.php
@@ -0,0 +1,13 @@
+<?php 
+define("BANDO_DE_DADOS", [
+'127.0.0.1',
+'root',
+'password',
+'test'
+
+
+
+]);
+print_r(BANDO_DE_DADOS);
+
+ ?>
---
+++ exemplo03.php
@@ -0,0 +1,11 @@
+<?php 
+echo PHP_OS_FAMILY;
+
+echo"<br>";
+
+
+echo DIRECTORY_SEPARATOR;
+
+
+
+ ?>
